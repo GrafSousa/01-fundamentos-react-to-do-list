9a400094485bc429023269e4fa0c9fe680ebccfb
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-02-10 11:57:49 -0300

    feature: add create task input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { PlusCircle } from '@phosphor-icons/react/dist/ssr';
+
 import './global.css';
 
 import styles from './App.module.css';
 
 import { Header } from './components/Header';
+import { Input } from './components/Input';
 function App() {
   return (
     <main>
       <Header />
+      <form className={styles.wrapper}>
+        <div className={styles.createTaskContainer}>
+          <Input
+            value={newTask}
+            placeholder='Adicione uma nova tarefa'
+            onChange={handleNewTaskChange}
+          />
+          <button
+            type='submit'
+            className={styles.createTaskButton}
+            disabled={isNewTaskEmpty}
+          >
+            Criar <PlusCircle size={16} />
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
